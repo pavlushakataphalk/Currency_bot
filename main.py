4fef1ac60d27e27de7b47956441150b071dc7d4e
pavlushakataphalk <129652719+pavlushakataphalk@users.noreply.github.com>
2023-04-05 23:45:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from bs4 import BeautifulSoup
+import datetime
+
 EURO_RUB = 'https://www.google.com/search?q=euro+to+ruble&newwindow=1&sxsrf=APwXEde8pFrfqho1nOYcU2NFF-_YaVBCCg%3A1680350839745&ei=dx4oZP_5LLKB9u8P_v-OwAs&ved=0ahUKEwi_kZrX0oj-AhWygP0HHf6_A7gQ4dUDCA8&uact=5&oq=euro+to+ruble&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIMCAAQigUQQxBGEIICMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEOgoIABBHENYEELADOgoIABCKBRCwAxBDOgQIIxAnOgoILhDHARDRAxAnOgsILhCABBDHARDRAzoHCAAQigUQQzoHCC4QigUQQzoQCC4QigUQxwEQ0QMQ1AIQQzoHCAAQgAQQCjoKCAAQgAQQFBCHAjoPCAAQgAQQFBCHAhBGEIICSgQIQRgAUIEIWLIeYL4iaAFwAXgAgAH0AYgBrAySAQU1LjcuMZgBAKABAcgBCsABAQ&sclient=gws-wiz-serp'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
 
@@ -27,13 +29,16 @@ mykb = InlineKeyboardMarkup(row_width=1)
 mybt = InlineKeyboardButton(text="Обновить курс", callback_data="refresh")
 mykb.add(mybt)
 
-@dp.message_handler(commands=['start'])
+@dp.message_handler(commands=['start','refresh'])
 async def cmd_start(message: types.Message):
     await message.reply(get_value(), reply_markup=mykb)
 
 @dp.callback_query_handler(text="refresh")
 async def send_welcome(query: types.CallbackQuery):
-    await query.message.edit_text(get_value(), reply_markup=mykb)
+    if query.message.text != get_value():
+     await query.message.edit_text(get_value(), reply_markup=mykb)
+
+    await query.answer("Курс обновлен " + datetime.now.strftime("%d/%m/%Y, %H:%M"))
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
