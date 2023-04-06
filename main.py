a2b8771c7906ea2f068796aaa2560037aa37446b
pavlushakataphalk <129652719+pavlushakataphalk@users.noreply.github.com>
2023-04-06 18:52:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from bs4 import BeautifulSoup
+import datetime
+
 EURO_RUB = 'https://www.google.com/search?q=euro+to+ruble&newwindow=1&sxsrf=APwXEde8pFrfqho1nOYcU2NFF-_YaVBCCg%3A1680350839745&ei=dx4oZP_5LLKB9u8P_v-OwAs&ved=0ahUKEwi_kZrX0oj-AhWygP0HHf6_A7gQ4dUDCA8&uact=5&oq=euro+to+ruble&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIMCAAQigUQQxBGEIICMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEOgoIABBHENYEELADOgoIABCKBRCwAxBDOgQIIxAnOgoILhDHARDRAxAnOgsILhCABBDHARDRAzoHCAAQigUQQzoHCC4QigUQQzoQCC4QigUQxwEQ0QMQ1AIQQzoHCAAQgAQQCjoKCAAQgAQQFBCHAjoPCAAQgAQQFBCHAhBGEIICSgQIQRgAUIEIWLIeYL4iaAFwAXgAgAH0AYgBrAySAQU1LjcuMZgBAKABAcgBCsABAQ&sclient=gws-wiz-serp'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
 
@@ -16,9 +18,11 @@ def get_value():
     fin = "Актуальный курс Евро (EUR):\n" + curreu + "\n" + "Актуальный курс Сербского Динара(RSD):\n"+ currdin
     return fin
 
+
 get_value()
 
-API_TOKEN = '5653760842:AAHlXeWckFJj_OVHixjSt1ySw9ps6yPYH_g'
+API_TOKEN = '6285702618:AAHbyuoUBGiJ0v9fmc0QZvZvAwNyFYKkNIo'
+
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
@@ -33,6 +37,11 @@ async def cmd_start(message: types.Message):
 
 @dp.callback_query_handler(text="refresh")
 async def send_welcome(query: types.CallbackQuery):
+    if query.message.text != get_value():
+     await query.message.edit_text(get_value(), reply_markup=mykb)
+
+    date = datetime.datetime.now().strftime("%d/%m/%Y, %H:%M")
+    await query.answer("Курс обновлен " + date)
     await query.message.edit_text(get_value(), reply_markup=mykb)
 
 if __name__ == '__main__':
